ba0f1f07d38e77e734a94e9a91172ffcb0c6a625
nunyabees08 <8467063-nunyabees08@users.noreply.replit.com>
2023-09-29 22:51:21 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+#access libraries and py files 
+import spiderDraw.py as sd
+#Initialize variables and setup 
+#Need to keep track of correct letters, incorrect letters and tries
+
+
+
+#Make a list of the spider drawings
+spiderList = [sd.spider_0(), sd.spider_1(), sd.spider_2(), sd.spider_3(), sd.spider_4(), sd.spider_5(), sd.spider_6() ]
+
+
+#Print intro statements (welcome to game, etc)
+
+
+
+#generate a random word from word list
+
+
+
+
+#Game Loop
+
+
+  #This is where you'll call all of your functions. Just need to decide the proper order.
+
+
+  #You will also need to specify your win and lose conditions in here
